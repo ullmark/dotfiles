321755c3ae7b417a5ca198a08693f8a0726c0ebf
Markus Ullmark <ullmark@gmail.com>
2013-03-14 10:32:32 +0100

    moved homebrew to the top of the PATH to make it use the correct git..

--- bash/path.sh
+++ bash/path.sh
@@ -1,5 +1,7 @@
 # RVM
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" 
+# Homebrew
+export PATH=/usr/local/bin:$PATH
 export PATH=~/.bin:$PATH
 export PATH=/usr/local/sbin:$PATH
 export PATH=/usr/local/share/npm/bin:$PATH
