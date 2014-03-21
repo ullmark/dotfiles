ed7e4222f2a2aecd9cdc137b2a55476f3daac372
Markus Ullmark <ullmark@gmail.com>
2014-03-21 11:13:01 +0100

    using nvm nowadays

--- shared/path.sh
+++ shared/path.sh
@@ -7,7 +7,6 @@
 export PATH=/usr/local/bin:$PATH
 export PATH=~/.bin:$PATH
 export PATH=/usr/local/sbin:$PATH
-export PATH=/usr/local/share/npm/bin:$PATH
 
 ### Added by the Heroku Toolbelt
 export PATH="/usr/local/heroku/bin:$PATH"
