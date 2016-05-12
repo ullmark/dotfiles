de168a89186ddcd01d0a478fe00b45e63abd87ac
Markus Ullmark <ullmark@gmail.com>
2016-05-12 19:44:17 +0200

    fixed rvm, added comment and moved the wrongfully placed dotnet version manager thingie

--- shared/path.sh
+++ shared/path.sh
@@ -1,8 +1,13 @@
-# RVM
-# [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+# RVM (ruby version manager)
+# note: if you get an error "__rvm_cleanse_variables" in zsh see:
+# http://stackoverflow.com/questions/10585002/zsh-complains-about-rvm-rvm-cleanse-variables-function-definition-file-not-fo
+[ -s "$HOME/.rvm/scripts/rvm" ] && . $HOME/.rvm/scripts/rvm
 
-# NVM
+# NVM (node version manager)
 [ -s $HOME/.nvm/nvm.sh ] && . $HOME/.nvm/nvm.sh # This loads NVM
+
+# DNVM (dotnet version manager)
+[ -s $HOME/.dnx/dnvm/dnvm.sh ] && . $HOME/.dnx/dnvm/dnvm.sh # Load dnvm
 
 # Homebrew
 export PATH=$PATH:/usr/local/bin
